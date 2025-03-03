3c4249014dc362b93b46af9954b5bd73c92444f9
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-03 15:32:05 +0900

    commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
-import {DarkThemeToggle} from "flowbite-react";
 import Editor from "./modules/Editor";
 
 function App() {
     return (
-        <main>
-            <DarkThemeToggle/>
+        <main className="h-screen">
             <Editor/>
+
+            <footer className="text-center py-3 text-sm text-gray-500">
+                jjjjj
+            </footer>
         </main>
     );
 }
--- client/src/modules/Editor.tsx
+++ client/src/modules/Editor.tsx
@@ -1,6 +1,7 @@
 import {Editor} from "@monaco-editor/react";
 import {useRef, useState} from "react";
-import {Button} from "flowbite-react";
+import {Button, DarkThemeToggle} from "flowbite-react";
+import * as monaco from "monaco-editor";
 
 const defaultCode = `package main
 
@@ -12,27 +13,41 @@ func main() {
 
 export default function Component() {
     const [code, setCode] = useState(defaultCode)
-    const editorRef = useRef(null);
 
-    function handleEditorDidMount(editor, monaco) {
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+    function onEditorMount(editor: monaco.editor.IStandaloneCodeEditor) {
         editorRef.current = editor;
+    }
+
+    function onChange(code: string = "") {
+        setCode(code);
     }
 
     return (
         <>
-            <div className="flex gap-2 mb-2 px-2 justify-end">
-                <Button>VIM</Button>
-                <Button>VIM</Button>
+            <div className="flex justify-between items-center py-2 px-3">
+                <h1 className="text-2xl font-bold">Editor</h1>
+
+                <div className="flex gap-2 justify-end">
+                    <Button size={"sm"} color={"red"}>VIM</Button>
+                    <Button size={"sm"} color={"red"}>Auto Run</Button>
+                    <Button size={"sm"} gradientDuoTone={"greenToBlue"}>Export</Button>
+                    <DarkThemeToggle/>
+                </div>
             </div>
+
             <Editor
-                height="90vh"
                 theme="vs-dark"
+                height="calc(100vh - 100px)"
                 defaultLanguage="go"
                 defaultValue={code}
                 options={{
-                    fontSize: 14
-            }}
-                onMount={handleEditorDidMount}
+                    fontSize: 14,
+                    minimap: {enabled: false}
+                }}
+                onChange={onChange}
+                onMount={onEditorMount}
             />
         </>
     );
