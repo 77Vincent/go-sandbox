9d68ad00a0a6a2361d374e071c7c177cc45ea8ff
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-17 11:11:36 +0800

    commit

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 	r := gin.Default()
 
 	// a global timeout middleware as a safety net
-	r.Use(internal.Timeout(config.APIGlobalTimeout * time.Second))
+	timeout := internal.Timeout(config.APIGlobalTimeout * time.Second)
 
 	// routes
-	r.GET("/status", internal.Status)
-	r.POST("/format", internal.Format)
+	r.GET("/status", timeout, internal.Status)
+	r.POST("/format", timeout, internal.Format)
 	r.POST("/execute", internal.Execute)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
