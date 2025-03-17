497d466537a5ab06e1961e63a5d20f2cc52d9574
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-17 15:02:06 +0800

    commit

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -9,6 +9,9 @@ export async function healthCheck() {
     return await res.json();
 }
 
+const SSE_EVENT_KEY = "event:"
+const SSE_DATA_KEY = "data:"
+
 export async function* executeCodeStream(code: string): AsyncGenerator<SSEEvent, void, unknown> {
     const res = await fetch("/api/execute", {
         method: "POST",
@@ -33,36 +36,38 @@ export async function* executeCodeStream(code: string): AsyncGenerator<SSEEvent,
     let buffer = "";
 
     while (true) {
-        const { done, value } = await reader.read();
+        const {done, value} = await reader.read();
         if (done) break;
 
         // 将读取到的数据解码并累加到 buffer 中
-        buffer += decoder.decode(value, { stream: true });
+        buffer += decoder.decode(value, {stream: true});
         // SSE 事件以两个换行符 "\n\n" 作为分隔符
         const parts = buffer.split("\n\n");
         // 最后一部分可能不完整，保留到下一轮处理
         buffer = parts.pop() || "";
 
         for (const part of parts) {
-            const lines = part.split("\n").filter(line => line.trim() !== "");
-            let eventName = "message";
+            const lines = part.split("\n");
+
+            let event = "message";
             let data = "";
+
             for (const line of lines) {
-                if (line.startsWith("event:")) {
-                    eventName = line.substring("event:".length).trim();
-                } else if (line.startsWith("data:")) {
-                    data += line.substring("data:".length).trim() + "\n";
+                if (line.startsWith(SSE_EVENT_KEY)) {
+                    event = line.substring(SSE_EVENT_KEY.length).trim();
+                } else if (line.startsWith(SSE_DATA_KEY)) {
+                    data += line.substring(SSE_DATA_KEY.length).trim()
+                } else if (line !== "") {
+                    // it was a \n before split and supposed to be part of the previous data
+                    yield {event: "stdout", data: "\n"};
                 }
             }
-            if (data.endsWith("\n")) {
-                data = data.slice(0, -1);
-            }
-            yield { event: eventName, data };
+
+            yield {event: event, data};
         }
     }
-
-    return
 }
+
 
 export async function formatCode(code: string): Promise<ExecuteResultI> {
     const res = await fetch("/api/format", {
