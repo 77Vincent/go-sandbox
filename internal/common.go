fd34222fc9958cee6c01d13f70ff4e32d1937d0c
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-21 23:40:31 +0900

    commit

--- internal/common.go
+++ internal/common.go
@@ -3,9 +3,39 @@ package internal
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"net/http"
 	"time"
 )
+
+const (
+	badRequestMessage = "bad request"
+	buildErrorMessage = "build failed"
+)
+
+var rdb = redis.NewClient(&redis.Options{
+	Addr: "redis:6379", // Redis server address
+	// Password: "", // no password set
+	// DB:       0,  // use default DB
+})
+
+func Status(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+type request struct {
+	Code    string `json:"code" binding:"required"`
+	Version string `json:"version"`
+}
+
+type response struct {
+	Stderr  string `json:"stderr"`
+	Stdout  string `json:"stdout"`
+	Error   string `json:"error"`
+	Message string `json:"message"`
+}
 
 // Timeout creates a middleware that enforces a timeout.
 func Timeout(timeout time.Duration) gin.HandlerFunc {
