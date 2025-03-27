62e0bb8a9033182f5a005f4ae149c5db00ff9c4e
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-27 15:43:35 +0900

    commit

--- client/src/modules/About.tsx
+++ client/src/modules/About.tsx
@@ -1,7 +1,10 @@
 import {Button, Modal} from "flowbite-react";
+import {FaGithub, FaCoffee} from "react-icons/fa";
+
 import {TITLE, TRANSLATE} from "../constants.ts";
 import {languages} from "../types";
 import {Divider} from "./Common.tsx";
+import {Link} from "react-router";
 
 export default function Component(props: {
     show: boolean,
@@ -10,7 +13,7 @@ export default function Component(props: {
 }) {
     const {show, setShow, lan} = props
 
-    return <Modal className={"dark:text-gray-50"} dismissible show={show} onClose={() => setShow(false)}>
+    return <Modal dismissible show={show} onClose={() => setShow(false)}>
         <Modal.Header>
             <div className={"flex items-center gap-2"}>
                 <img src={"/logo.svg"} alt={"logo"} className={"h-5 max-md:hidden"}/>
@@ -19,15 +22,32 @@ export default function Component(props: {
         </Modal.Header>
 
         <Modal.Body className={"flex flex-col items-center justify-between gap-4"}>
-            <p>
+            <p className={"dark:text-gray-50"}>
                 {TRANSLATE.aboutInfo[lan]}
             </p>
 
             <Divider horizontal={true}/>
 
-            <div className="flex flex-wrap justify-center gap-4">
-                <Button size={"sm"} href={"https://www.buymeacoffee.com/"} target={"_blank"}>
-                    {TRANSLATE.coffee[lan]} (1$)
+            <div className="flex flex-wrap justify-center gap-3">
+                <Button size={"xs"}
+                        as={Link}
+                        className={"hover:opacity-80"}
+                        gradientMonochrome={"cyan"}
+                        to={"https://www.buymeacoffee.com/"} target={"_blank"}>
+                    <div className={"flex items-center gap-2"}>
+                        <FaCoffee size={20}/>
+                        {TRANSLATE.coffee[lan]}
+                    </div>
+                </Button>
+
+                <Button size={"xs"}
+                        as={Link}
+                        className={"hover:opacity-80"}
+                        to={"https://github.com/77Vincent/go-sandbox"} target={"_blank"}>
+                    <div className={"flex items-center gap-2"}>
+                        <FaGithub size={20}/>
+                        GitHub
+                    </div>
                 </Button>
             </div>
         </Modal.Body>
