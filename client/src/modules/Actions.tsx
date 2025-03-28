7c6bcd2093f784c6d08f9a47052f040d37d00402
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-28 21:45:55 +0900

    commit

--- client/src/modules/Actions.tsx
+++ client/src/modules/Actions.tsx
@@ -1,13 +1,12 @@
 import {Tooltip} from "flowbite-react";
-import {PiPlay as RunICon} from "react-icons/pi";
-import {HiCodeBracket as FormatIcon} from "react-icons/hi2";
+import {FiPlay as RunICon} from "react-icons/fi";
+import {HiMiniCodeBracket as FormatIcon} from "react-icons/hi2";
 import {RiShareBoxLine as ShareIcon} from "react-icons/ri";
 
 import {HOVER_CLASS, TRANSLATE} from "../constants.ts";
 import {languages} from "../types";
 import {isMac} from "../utils.ts";
 
-const BUTTON_SIZE = 23;
 const COLOR_INACTIVE = "text-gray-300 dark:text-gray-600";
 const CMD = "Cmd"
 const WIN = "win"
@@ -28,17 +27,17 @@ export default function Component(props: {
         <>
             <Tooltip className={COMMON_CLASSES} content={`${TRANSLATE.run[lan]}: ${metaKey} + enter`}>
                 <RunICon className={hasCode && !isRunning ? HOVER_CLASS : COLOR_INACTIVE}
-                         onClick={hasCode && !isRunning ? debouncedRun : undefined} size={BUTTON_SIZE}/>
+                         onClick={hasCode && !isRunning ? debouncedRun : undefined} size={23}/>
             </Tooltip>
 
             <Tooltip className={COMMON_CLASSES} content={`${TRANSLATE.format[lan]}: ${metaKey} + b`}>
                 <FormatIcon className={`mx-1.5 max-md:mx-0.5 ${hasCode && !isRunning ? HOVER_CLASS : COLOR_INACTIVE}`}
-                            onClick={hasCode && !isRunning ? debouncedFormat : undefined} size={BUTTON_SIZE}/>
+                            onClick={hasCode && !isRunning ? debouncedFormat : undefined} size={23}/>
             </Tooltip>
 
             <Tooltip className={COMMON_CLASSES} content={`${TRANSLATE.share[lan]}: ${metaKey} + e`}>
                 <ShareIcon className={hasCode ? HOVER_CLASS : COLOR_INACTIVE}
-                           onClick={hasCode ? debouncedShare : undefined} size={BUTTON_SIZE}/>
+                           onClick={hasCode ? debouncedShare : undefined} size={22}/>
             </Tooltip>
         </>
     );
--- client/src/modules/Editor.tsx
+++ client/src/modules/Editor.tsx
@@ -486,7 +486,7 @@ export default function Component(props: {
                 <Link to={""} className={"flex items-center gap-2 transition-opacity duration-300 hover:opacity-70"}>
                     <img src={"/logo.svg"} alt={"logo"} className={"h-5 max-md:hidden"}/>
 
-                    <div className="text-2xl italic text-gray-700 dark:text-cyan-500 max-md:text-base">{TITLE}</div>
+                    <div className="text-2xl font-semibold text-gray-800 dark:text-gray-200 max-md:text-base">{TITLE}</div>
                 </Link>
 
                 <div className="flex items-center justify-end gap-2.5 max-md:gap-1">
