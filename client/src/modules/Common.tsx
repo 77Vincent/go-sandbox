5617331d9b1b6e65fb1c88622740d6109d377532
tianqi.wen <tianqi.wen@fastretailing.com>
2025-03-29 23:36:57 +0900

    commit

--- client/src/modules/Common.tsx
+++ client/src/modules/Common.tsx
@@ -83,7 +83,7 @@ export function ToggleSwitch(props: {
             <div>
                 <input checked={checked} onChange={onChange} type="checkbox" value="" className="peer sr-only"/>
                 <div
-                    className="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:size-5 after:rounded-full after:border after:border-neutral-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-cyan-500 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-cyan-300 dark:border-neutral-600 dark:bg-gray-700 dark:peer-focus:ring-cyan-800 rtl:peer-checked:after:-translate-x-full"></div>
+                    className="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:size-5 after:rounded-full after:border after:border-neutral-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-cyan-500 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-cyan-300 dark:border-neutral-600 dark:bg-gray-500 dark:peer-focus:ring-cyan-800 rtl:peer-checked:after:-translate-x-full"></div>
             </div>
 
             {
