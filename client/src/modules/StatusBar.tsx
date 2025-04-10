698116c884c7cd661e8a6a6875cbf9818532e31d
tianqi.wen <tianqi.wen@fastretailing.com>
2025-04-10 16:30:17 +0900

    commit

--- client/src/modules/StatusBar.tsx
+++ client/src/modules/StatusBar.tsx
@@ -3,7 +3,7 @@ import {CiFaceFrown as ErrorIcon, CiFaceSmile as GoodIcon} from "react-icons/ci"
 const errorClasses = "text-orange-800 dark:text-orange-700";
 const goodClasses = "text-cyan-700 dark:text-cyan-500";
 const warningClasses = "text-yellow-800 dark:text-yellow-700";
-const textClasses = "text-xs text-gray-700 dark:text-gray-300";
+const textClasses = "text-xs text-gray-700 dark:text-gray-400";
 
 export default function Component(props: {
     row: number,
@@ -17,12 +17,14 @@ export default function Component(props: {
     return (
         <div
             className={"fixed bottom-0 left-0 z-10 flex w-full justify-between border-t border-t-gray-400 bg-gray-200 px-3 py-0.5 dark:border-t-gray-600 dark:bg-gray-900 "}>
-            <div className={textClasses}>
-                {row}:{col}
+            <div className={`flex items-center gap-1 ${textClasses}`}>
+                <img src={"/logo.svg"} alt={"logo"} className={"h-2"}/>
+                /main.go
             </div>
 
 
             <div className={"flex items-center gap-4"}>
+                <span className={textClasses}> {row}:{col} </span>
                 <span className={textClasses}>4 spaces</span>
 
                 <div className={"flex items-center gap-1"}>
