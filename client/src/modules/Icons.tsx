16b656ee18a749e65c877335e052caeb4dc00d18
tianqi.wen <tianqi.wen@fastretailing.com>
2025-05-07 16:03:45 +0900

    commit

--- client/src/modules/Icons.tsx
+++ client/src/modules/Icons.tsx
@@ -1,11 +1,18 @@
 import {isMac} from "../utils.ts";
 import {MdKeyboardCommandKey, MdKeyboardControlKey} from "react-icons/md";
 
+export {ImTextColor as TextMIcon} from "react-icons/im"
+
 export {IoMdRemoveCircleOutline as RemoveIcon} from "react-icons/io";
 
 export {IoClose as CloseIcon} from "react-icons/io5";
 export {CiFaceFrown as BadIcon, CiFaceSmile as GoodIcon} from "react-icons/ci";
-export {VscSettingsGear as SettingsIcon} from "react-icons/vsc";
+export {
+    VscSettingsGear as SettingsIcon,
+    VscLayoutPanel as LayoutVerticalIcon,
+    VscLayoutSidebarRight as LayoutHorizontalIcon,
+} from "react-icons/vsc";
+
 export {
     HiOutlineInformationCircle as AboutIcon,
     HiOutlineQuestionMarkCircle as ManualIcon,
@@ -14,13 +21,18 @@ export {
     HiRefresh as RefreshIcon,
     HiMail as MailIcon,
 } from "react-icons/hi";
+
 export {
     BiCut as CutIcon,
     BiSearch as SearchIcon,
 } from "react-icons/bi";
+
 export {RiShareBoxLine as ShareIcon} from "react-icons/ri";
+
 export {HiMiniCodeBracket as FormatIcon} from "react-icons/hi2";
+
 export {FiPlay as RunICon} from "react-icons/fi";
+
 export {
     MdOutlineContentPaste as PasteIcon,
     MdContentCopy as CopyIcon,
@@ -29,7 +41,10 @@ export {
     MdKeyboardReturn as EnterKey,
     MdOutlineAdd as AddIcon,
     MdOutlineEdit as EditIcon,
+    MdTextDecrease as TextSIcon,
+    MdTextIncrease as TextLIcon,
 } from "react-icons/md";
+
 export {BsShift as ShiftKey} from "react-icons/bs";
 
 export function MetaKey() {
