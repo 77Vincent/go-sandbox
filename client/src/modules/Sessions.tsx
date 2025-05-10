e3d8eabbe598c9dc255d911719ec589f64b058bb
tianqi.wen <tianqi.wen@fastretailing.com>
2025-05-10 11:27:52 +0900

    commit

--- client/src/modules/Sessions.tsx
+++ client/src/modules/Sessions.tsx
@@ -4,7 +4,7 @@ import {MouseEventHandler} from "react";
 import {ACTIVE_ICON_BUTTON_CLASS_2} from "../constants.ts";
 import {CloseIcon} from "./Icons.tsx";
 
-const activeClasses = "border-b-cyan-500 border-b-2 dark:text-white"
+const activeClasses = "border-b-cyan-500 border-b-2 bg-white dark:bg-neutral-800 dark:text-white"
 const inactiveClasses = "text-gray-800 dark:text-gray-300"
 
 function Session(props: {
@@ -54,7 +54,7 @@ function Session(props: {
     return (
         <div
             onClick={handleClick(id)}
-            className={`flex h-8 cursor-default items-center gap-1 border-r border-r-gray-100 px-2 py-1.5 hover:bg-gray-100 dark:border-r-gray-800 dark:hover:bg-gray-700 ${active ? activeClasses : inactiveClasses}`}>
+            className={`flex h-8 cursor-default items-center gap-1 border-r border-r-gray-200 px-2 py-1.5 hover:bg-gray-200 dark:border-r-gray-800 dark:hover:bg-gray-700 ${active ? activeClasses : inactiveClasses}`}>
             <span className={"max-w-32 truncate text-xs italic tracking-wide"}>{displayFileUri(id)}</span>
             {
                 num > 0 &&
@@ -76,7 +76,7 @@ export function Sessions(props: {
 
     return (
         <div
-            className={`relative z-10 flex items-center overflow-x-auto border-b border-gray-300 shadow dark:border-gray-700 ${mode === "dark" ? "editor-bg-dark" : ""}`}>
+            className={`relative z-10 flex items-center overflow-x-auto border-b border-gray-300 bg-gray-100 shadow dark:border-gray-700 ${mode === "dark" ? "editor-bg-dark" : ""}`}>
             {sessions.map(({id}, i) => {
                     return <Session num={i}
                                     sessions={sessions.map(v => v.id)}
