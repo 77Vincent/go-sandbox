73078bdfbbb76a88b0e78aab5889851abd68ea40
tianqi.wen <tianqi.wen@fastretailing.com>
2025-05-17 12:23:32 +0900

    commit

--- client/src/modules/Drawer.tsx
+++ client/src/modules/Drawer.tsx
@@ -9,10 +9,17 @@ import {
 import {countSymbols, SYMBOL_KIND_MAP} from "../lib/lsp.ts";
 import {TRANSLATE} from "../lib/i18n.ts";
 import {Divider, Row} from "./Common.tsx";
-import {CloseIcon, FoldIcon, UnfoldIcon} from "./Icons.tsx";
-import {useCallback, useContext, useState} from "react";
+import {CloseIcon, FoldIcon, SearchIcon, UnfoldIcon} from "./Icons.tsx";
+import {ChangeEvent, useCallback, useContext, useState} from "react";
 import {AppCtx, isUserCode} from "../utils.ts";
 import {IconButton} from "./IconButton.tsx";
+import {TextInput} from "flowbite-react";
+
+function SearchIconWrapper() {
+    return (
+        <SearchIcon className={"opacity-50"}/>
+    )
+}
 
 const symbolStyle = (kind: number): string => {
     switch (kind) {
@@ -50,6 +57,8 @@ export default function Component(props: {
         lines,
     } = props;
     const {lan, file, isRunning, openedDrawer, updateOpenedDrawer} = useContext(AppCtx)
+    const [searchSymbol, setSearchSymbol] = useState("");
+    const [searchSnippet, setSearchSnippet] = useState("");
     const [foldedSnippetSections, setFoldedSnippetSections] = useState<Record<string, boolean>>({})
 
     const closeDrawer = () => {
@@ -84,6 +93,13 @@ export default function Component(props: {
         }
     }, []);
 
+    const onSearchDocumentSymbols = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setSearchSymbol(e.target.value);
+    }, [setSearchSymbol]);
+    const onSearchSnippets = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setSearchSnippet(e.target.value);
+    }, [setSearchSnippet]);
+
     const displaySymbols = Object.entries(countSymbols(documentSymbols))
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         .filter(([_, value]) => {
@@ -95,31 +111,47 @@ export default function Component(props: {
             className={"relative z-10 flex h-full flex-col overflow-y-auto border-r border-r-gray-400 dark:border-gray-600"}>
 
             <div
-                className={"sticky top-0 border-b border-b-gray-300 bg-gray-100 py-2 shadow dark:border-b-gray-700 dark:bg-neutral-900"}>
+                className={"sticky top-0 border-b border-b-gray-300 bg-gray-100 py-2.5 shadow dark:border-b-gray-700 dark:bg-neutral-900"}>
                 <div
                     className={"flex items-center justify-between px-2 text-xs font-semibold text-gray-900 dark:text-gray-100"}>
                     {openedDrawer && TRANSLATE[openedDrawer][lan]}
                     <IconButton icon={<CloseIcon size={14}/>} variant={"secondary"} onClick={closeDrawer}/>
                 </div>
+
+                {
+                    (openedDrawer === DRAWER_DOCUMENT_SYMBOLS || openedDrawer === DRAWER_LIBRARY) && (
+                        <div className={"mx-2 mt-2.5"}>
+                            <TextInput value={searchSymbol}
+                                       className={openedDrawer === DRAWER_DOCUMENT_SYMBOLS ? "" : "hidden"}
+                                       onChange={onSearchDocumentSymbols} icon={SearchIconWrapper} sizing={"sm"}
+                                       placeholder={"search"}/>
+                            <TextInput value={searchSnippet} className={openedDrawer === DRAWER_LIBRARY ? "" : "hidden"}
+                                       onChange={onSearchSnippets} icon={SearchIconWrapper} sizing={"sm"}
+                                       placeholder={"search"}/>
+                        </div>
+                    )
+                }
             </div>
 
             <div className={"flex flex-1 flex-col overflow-x-auto bg-neutral-50 pb-6 dark:bg-neutral-900"}>
                 {
-                    openedDrawer === DRAWER_DOCUMENT_SYMBOLS && documentSymbols.map(({name, kind}, index) => {
-                        return (
-                            <div key={name} onClick={onSymbolClick(index)}
-                                 className={LINE_STYLE}
-                            >
-                                <div className={`flex items-center gap-1 truncate ${symbolStyle(kind)}`}>
-                                    {name}
-                                </div>
+                    openedDrawer === DRAWER_DOCUMENT_SYMBOLS && documentSymbols
+                        .filter(({name}) => name.toLowerCase().includes(searchSymbol.toLowerCase()))
+                        .map(({name, kind}, index) => {
+                            return (
+                                <div key={name} onClick={onSymbolClick(index)}
+                                     className={LINE_STYLE}
+                                >
+                                    <div className={`flex items-center gap-1 truncate ${symbolStyle(kind)}`}>
+                                        {name}
+                                    </div>
 
-                                <div className={"font-light text-gray-400 dark:text-gray-500"}>
-                                    {SYMBOL_KIND_MAP[kind] ?? kind}
+                                    <div className={"font-light text-gray-400 dark:text-gray-500"}>
+                                        {SYMBOL_KIND_MAP[kind] ?? kind}
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })
+                            );
+                        })
                 }
                 {
                     openedDrawer === DRAWER_STATS && (
@@ -154,37 +186,40 @@ export default function Component(props: {
                     openedDrawer == DRAWER_LIBRARY && (
                         <>
                             {
-                                Object.keys(SNIPPETS).map(key => {
-                                    return (
-                                        <div key={key}>
-                                            <Row onClick={foldSection(key)}
-                                                 className={"cursor-pointer border-b p-2 pr-1.5 shadow-sm dark:border-b-gray-700"}>
-                                                <div key={key}
-                                                     className={"text-sm font-semibold text-black  dark:text-white"}>{key}</div>
-                                                <IconButton
-                                                    icon={foldedSnippetSections[key] ? <UnfoldIcon/> : <FoldIcon/>}/>
-                                            </Row>
+                                Object.keys(SNIPPETS)
+                                    .map(key => {
+                                        return (
+                                            <div key={key}>
+                                                <Row onClick={foldSection(key)}
+                                                     className={"cursor-pointer border-b p-2 pr-1.5 dark:border-b-gray-700"}>
+                                                    <div key={key}
+                                                         className={"text-sm font-semibold text-black  dark:text-white"}>{key}</div>
+                                                    <IconButton
+                                                        icon={foldedSnippetSections[key] ? <UnfoldIcon/> : <FoldIcon/>}/>
+                                                </Row>
 
-                                            <div
-                                                className={`border-b text-gray-900 dark:border-b-gray-700 dark:text-gray-200 ${foldedSnippetSections[key] ? "hidden" : ""}`}>
-                                                {
-                                                    Object.entries(SNIPPETS[key]).map(([subkey, value]) => {
-                                                        return (
-                                                            <div
-                                                                key={subkey}
-                                                                className={`${LINE_STYLE} ${isRunning || !isUserCode(file) ? "opacity-50" : ""}`}
-                                                                onClick={onSnippetClick(subkey)}
-                                                            >
+                                                <div
+                                                    className={`border-b text-gray-900 dark:border-b-gray-700 dark:text-gray-200 ${foldedSnippetSections[key] ? "hidden" : ""}`}>
+                                                    {
+                                                        Object.entries(SNIPPETS[key])
+                                                            .filter(([subkey]) => subkey.toLowerCase().includes(searchSnippet.toLowerCase()))
+                                                            .map(([subkey, value]) => {
+                                                                return (
+                                                                    <div
+                                                                        key={subkey}
+                                                                        className={`${LINE_STYLE} ${isRunning || !isUserCode(file) ? "opacity-50" : ""}`}
+                                                                        onClick={onSnippetClick(subkey)}
+                                                                    >
 
-                                                                {value}
-                                                            </div>
-                                                        )
-                                                    })
-                                                }
+                                                                        {value}
+                                                                    </div>
+                                                                )
+                                                            })
+                                                    }
+                                                </div>
                                             </div>
-                                        </div>
-                                    )
-                                })
+                                        )
+                                    })
                             }
                         </>
                     )
